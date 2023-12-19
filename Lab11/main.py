2c48dfc6c0447fb1d01ebcb11c1334897ce60173
BaciuMaria <100144074+BaciuMaria@users.noreply.github.com>
2023-12-19 13:41:19 +0200

    Update main.py

--- Lab11/main.py
+++ Lab11/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pymc as pm
 import arviz as az
 import matplotlib.pyplot as plt
+from scipy import stats
 
 # Exercitiul 1
 
@@ -20,17 +21,48 @@ plt.show()
 
 # Exercitiul 2
 clusters = [2, 3, 4]
+models = []
+idatas = []
 
 for cluster in clusters:
     with pm.Model() as model:
-        mus = [pm.Normal(f'mu{i}', mu=0, tau=1/(102)) for i in range(cluster)]
-        taus = [pm.Gamma(f'tau_{i}', alpha=1, beta=1/(102)) for i in range(cluster)]
-        weights = pm.Dirichlet(f'weights', a=np.ones(cluster))
+        p = pm.Dirichlet('p', a=np.ones(cluster))
+        means = pm.Normal('means', mu=np.array(np.linspace(mix.min(), mix.max(), cluster)), sigma=10)
 
-        y = pm.Mixture(f'y', w=weights, comp_dists=[pm.Normal.dist(mu=m, tau=t) for m, t in zip(mus, taus)], observed=mix)
+        sigma = pm.HalfNormal('sigma', sigma=10)
+        y = pm.NormalMixture('y', w=p, mu=means, sigma=sigma, observed=mix)
 
-        trace = pm.sample(1000, tune=1000)
+        idata = pm.sample(1000, tune=2000, target_accept=0.9, random_seed=123, return_inferencedata=True)
+        idatas.append(idata)
+        models.append(model)
 
-    pm.plot_trace(trace)
-    plt.suptitle(f'Mixtura Gaussiana cu {cluster} componente')
-    plt.show()
+_, ax = plt.subplots(2, 2, figsize=(11, 8), constrained_layout=True)
+ax = np.ravel(ax)
+x = np.linspace(mix.min(), mix.max(), 200)
+
+for idx, idata_x in enumerate(idatas):
+    posterior_x = idata_x.posterior.stack(samples=("chain", "draw"))
+    x_ = np.array([x] * clusters[idx]).T
+
+    for i in range(50):
+        i_ = np.random.randint(0, posterior_x.samples.size)
+        means_y = posterior_x['means'][:, i_]
+        p_y = posterior_x['p'][:, i_]
+        sd = posterior_x['sigma'][i_]
+        dist = stats.norm(means_y, sd)
+        ax[idx].plot(x, np.sum(dist.pdf(x_) * p_y.values, 1), 'C0', alpha=0.1)
+
+    means_y = posterior_x['means'].mean("samples")
+    p_y = posterior_x['p'].mean("samples")
+    sd = posterior_x['sigma'].mean()
+    dist = stats.norm(means_y, sd)
+    ax[idx].plot(x, np.sum(dist.pdf(x_) * p_y.values, 1), 'C0', lw=2)
+    ax[idx].plot(x, dist.pdf(x_) * p_y.values, 'k--', alpha=0.7)
+
+    az.plot_kde(mix, plot_kwargs={'linewidth': 2, 'color': 'k'}, ax=ax[idx])
+
+    ax[idx].set_title('K = {}'.format(clusters[idx]))
+    ax[idx].set_yticks([])
+    ax[idx].set_xlabel('x')
+
+plt.show()
